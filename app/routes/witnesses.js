828f396fd9d59b047db3935715138177f1461007
Tom Kraak <tom.kraak@gmail.com>
2017-12-07 23:11:29 -0500

    create witness record

--- app/routes/witnesses.js
+++ app/routes/witnesses.js
@@ -1,4 +1,13 @@
+import Ember from 'ember';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    let witnessRecord = this.store.createRecord('witness', {
+      fName: 'Sam',
+      lName: 'Gandee',
+      email: 'test@test.com'
+    });
+    return [witnessRecord]
+  }
 });
