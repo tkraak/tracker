23def979515b77b807a0d24566573d5873f60f85
Tom Kraak <tom.kraak@gmail.com>
2017-12-07 22:14:41 -0500

    create sightings records

--- app/routes/sightings.js
+++ app/routes/sightings.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    let record1 = this.store.createRecord('sighting', {
+      location: 'Atlanta',
+      sightedAt: new Date('2017-02-09')
+    });
+
+    let record2 = this.store.createRecord('sighting', {
+      location: 'Baltimore',
+      sightedAt: new Date('2017-03-14')
+    });
+
+    let record3 = this.store.createRecord('sighting', {
+      location: 'New York',
+      sightedAt: new Date('2017-03-21')
+    });
+
+    return [record1, record2, record3];
+  }
 });
